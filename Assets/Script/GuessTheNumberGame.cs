fb3e39b85700218347fdfce2f965ba6160bbf3d3
Patrick Abdul-Ahad <patrick.abdul-ahad@outlook.com>
2019-02-02 18:06:21 +0000

    initial commit

---
+++ Assets/Script/GuessTheNumberGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessTheNumberGame : MonoBehaviour {
+
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int usersGuess;
+
+    void Start() {
+        guessNumber = Random.Range(0, 100);
+    }
+
+
+    public void CheckGuess() {
+        usersGuess = int.Parse(input.text);
+
+        if (usersGuess == guessNumber) {
+            infoText.text = "You Guessed The Number! You Are A Wizard";
+        } else if (usersGuess > guessNumber) {
+            infoText.text = "Go Lower!";
+        } else if (usersGuess < guessNumber) {
+            infoText.text = "Go Higher!";
+        }
+
+        input.text = "";
+    }
+} // class
